202017b81d8a1576834fb78ed879b7c4310a277e
Ethan Campbell <ethan.campbell46@gmail.com>
2020-08-25 20:56:16 -0400

    heyNow Method

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		public static void heyNow() {
+			String Msg = "Hey Now!";
+			System.out.println(Msg);
+		}
+
 		System.out.println("Hello from Eclipse");
 		System.out.println("Hello from GitHub");
 	}
